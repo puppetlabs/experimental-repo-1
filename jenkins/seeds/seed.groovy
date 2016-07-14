8ddabfbc4ee0a72b17d75cfb78abd1cd61145178
Wayne Warren <waynr+launchpad@sdf.org>
2016-07-14 15:06:54 -0700

    (QENG-3954) Grab branch name from environment.

--- jenkins/seeds/seed.groovy
+++ jenkins/seeds/seed.groovy
@@ -1,27 +1,27 @@
 def value_stream = "experimental"
 def owner_name = "waynr"
 def project_name = "experimental-repo-1"
-def branch_api = new URL("https://api.github.com/repos/${owner_name}/${project_name}/branches")
-def branches = new groovy.json.JsonSlurper().parse(branch_api.newReader())
+def branch_name = "${P_GIT_BRANCH_NAME}"
 
-branches.each {
-  def branch_name = it.name
-
-  pipelineJob("${value_stream}_${project_name}_workflow") {
-    scm {
-      git("git@github.com:${owner_name}/${project_name}.git")
-    }
-    triggers {
-      upstream("${value_stream}_experimental-repo-1_workflow_${branch_name}")
-      scm("H/5 * * * *")
-    }
-    definition {
-      cpsScm {
-        scm {
-          git("git@github.com:${owner_name}/${project_name}.git")
+pipelineJob("${value_stream}_${project_name}_workflow_${branch_name}") {
+  scm {
+    git("git@github.com:${owner_name}/${project_name}.git")
+  }
+  triggers {
+    scm("H/5 * * * *")
+  }
+  definition {
+    cpsScm {
+      scm {
+        git {
+          remote {
+            name("github")
+            url("git@github.com:${owner_name}/${project_name}.git")  
+          }
+          branch(branch_name)
         }
-        scriptPath("jenkins/pipelines/pipe-1.groovy")
       }
+      scriptPath("jenkins/pipelines/pipe-1.groovy")
     }
   }
 }
